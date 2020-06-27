addab494bedd134a0a98216f3099f0b928a2777f
Denzel Sapatin <46718580+dsapatin@users.noreply.github.com>
2020-06-26 23:08:52 -0400

    Add files via upload

--- public/javascript/m4.js
+++ public/javascript/m4.js
@@ -1,1 +1,31 @@
+//-------------Font Resizer--------------//
 
+function resizeText(multiplier) {
+    if (document.body.style.fontSize == "") {
+      document.body.style.fontSize = "1.0em";
+    }
+    document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "em";
+  
+    $("plustext").addEvent("click", function() {resizeText(1);});
+    $("minustext").addEvent("click", function() {resizeText(-1);});
+}
+
+//--------------End of Font Resizer----------//
+
+
+
+//--------------Background Switcher----------//
+
+$(document).ready(function() {
+    $('#background1').on('click', function() {
+        document.getElementById("backgr").style.backgroundImage = "url('bikers.jpg')";
+    })
+    $('#background2').on('click', function() {
+        document.getElementById("backgr").style.backgroundImage = "url('crowd.jpg')";
+    })
+    $('#background3').on('click', function() {
+        document.getElementById("backgr").style.backgroundImage = "url('laneoptions.jpg')";
+    })
+});
+
+//---------------End of Background Switcher--------//
